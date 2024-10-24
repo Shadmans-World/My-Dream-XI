9879c36ee04b64a80063ea5d16ac38ef9de6bfab
Shadmans-World <shaon10516045@gmail.com>
2024-10-24 21:23:41 +0600

    Project Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+const App = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      {/* Banner */}
+
+      {/* Players */}
+          {/* Available */}
+          {/* Selected */}
+      
+      {/* Newsletter */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default App;
